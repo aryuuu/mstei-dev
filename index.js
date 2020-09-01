2611913b54089a9cd10f9d5e5ff8895a429cb504
Faris Hasim <farishasimsyauqi@gmail.com>
2020-09-01 21:40:59 +0700

    second commit, tampilkan dua post pertama dari masing2 user

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ const main = async () => {
     const todos = await getToDo();
     const posts = await getPost();
 
-    const userToDo = [0,0,0,0,0,0,0,0,0,0];
+    const userToDo = [0,0,0,0,0,0,0,0,0,0]; 
+    // array userToDo untuk menyimpan data banyaknya todo yang belum diselesaikan user, 
+    // elemen ke-i menunjukan banyaknya ToDo yang belum diselesaikan oleh user dengan id i+1 
+
+    const firstTwoPost = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],
+                          [0,0,0],[0,0,0],[0,0,0],[0,0,0],
+                          [0,0,0],[0,0,0]];
+    // array firstTwoPost untuk menyimpan id dua post pertama dari masing masing user
+    // tiap elemen array berisi [id_post_pertama, id_post_kedua, banyaknya_post_yang_sudah_tercatat]
 
     todos.forEach((todo) => {
       if (!todo.completed) {
@@ -28,10 +36,25 @@ const main = async () => {
       }
     });
     
+    posts.forEach((post) => {
+      let id = post.userId - 1;
+      if (firstTwoPost[id][2] < 2) {
+        firstTwoPost[id][firstTwoPost[id][2]] = post.id;
+        firstTwoPost[id][2]++;
+      }
+    });
+
     console.log("List nama-nama user yang memiliki 10 atau lebih todo yang belum selesai");
     users.forEach((user) => {
-      if(userToDo[user.id-1] >= 10){
+      let id = user.id - 1;
+      if(userToDo[id] >= 10){
         console.log(`Nama User : ${user.name}`);
+        for(let i = 0;i < 2;i++) {
+          console.log(`Post ke-${i+1}: `);
+          console.log("Title : " + posts[(firstTwoPost[id][i]-1)].title);
+          console.log("Body : " + posts[(firstTwoPost[id][i]-1)].body);
+          console.log();
+        }
       }
     });
   } 
